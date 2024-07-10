1b5c2df329b3587d3ce262f5d0a2202009bf68b7
Cliff Brake <cbrake@bec-systems.com>
2024-07-10 16:34:17 -0400

    move some defines to overlays

--- apps/siot/src/main.c
+++ apps/siot/src/main.c
@@ -6,14 +6,12 @@
 
 LOG_MODULE_REGISTER(siot, LOG_LEVEL_DBG);
 
-/*
-HTTP_RESOURCE_DEFINE(ws_resource, test_http_service, "/", &ws_resource_detail);
+// HTTP_RESOURCE_DEFINE(ws_resource, test_http_service, "/", &ws_resource_detail);
 
-static uint16_t https_service_port = 80;
-HTTPS_SERVICE_DEFINE(siot_https_service, CONFIG_NET_CONFIG_MY_IPV4_ADDR,
-		     &https_service_port, 1, 10, NULL,
-		     sec_tag_list_verify_none, sizeof(sec_tag_list_verify_none));
-*/
+// static uint16_t https_service_port = 80;
+// HTTPS_SERVICE_DEFINE(siot_https_service, CONFIG_NET_CONFIG_MY_IPV4_ADDR,
+// 		     &https_service_port, 1, 10, NULL,
+// 		     sec_tag_list_verify_none, sizeof(sec_tag_list_verify_none));
 
 #define DAC_DEVICE_NODE DT_NODELABEL(dac)
 #define DAC_CHANNEL     0
@@ -24,14 +22,14 @@ int main(void)
 	LOG_INF("SIOT Zephyr Application! %s", CONFIG_BOARD_TARGET);
 	// http_server_start();
 
-	const struct device *dac_dev = DEVICE_DT_GET(DAC_DEVICE_NODE);
+	// const struct device *dac_dev = DEVICE_DT_GET(DAC_DEVICE_NODE);
 
-	if (!device_is_ready(dac_dev)) {
-		LOG_WRN("DAC device not ready\n");
-	}
+	// if (!device_is_ready(dac_dev)) {
+	// 	LOG_WRN("DAC device not ready\n");
+	// }
 
-	int ret = dac_write_value(dac_dev, DAC_CHANNEL, 256);
-	LOG_DBG("dac write returned: %i", ret);
+	// int ret = dac_write_value(dac_dev, DAC_CHANNEL, 256);
+	// LOG_DBG("dac write returned: %i", ret);
 
 	return 0;
 }
