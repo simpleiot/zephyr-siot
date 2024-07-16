b84373c246d1a93dd3f8927d8ee8988df9eeb37e
Cliff Brake <cbrake@bec-systems.com>
2024-07-16 17:23:26 -0400

    code to embed index.html and start service

--- apps/siot/src/main.c
+++ apps/siot/src/main.c
@@ -2,34 +2,36 @@
 #include <zephyr/net/http/server.h>
 #include <zephyr/net/http/service.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/drivers/dac.h>
 
 LOG_MODULE_REGISTER(siot, LOG_LEVEL_DBG);
 
-// HTTP_RESOURCE_DEFINE(ws_resource, test_http_service, "/", &ws_resource_detail);
+static uint8_t index_html_gz[] = {
+#include "index.html.gz.inc"
+};
 
-// static uint16_t https_service_port = 80;
-// HTTPS_SERVICE_DEFINE(siot_https_service, CONFIG_NET_CONFIG_MY_IPV4_ADDR,
-// 		     &https_service_port, 1, 10, NULL,
-// 		     sec_tag_list_verify_none, sizeof(sec_tag_list_verify_none));
+static uint16_t http_service_port = 80;
+HTTP_SERVICE_DEFINE(siot_http_service, CONFIG_NET_CONFIG_MY_IPV4_ADDR, &http_service_port, 1, 10,
+		    NULL);
 
-#define DAC_DEVICE_NODE DT_NODELABEL(dac)
-#define DAC_CHANNEL     0
-#define DAC_RESOLUTION  8
+struct http_resource_detail_static index_html_gz_resource_detail = {
+	.common =
+		{
+			.type = HTTP_RESOURCE_TYPE_STATIC,
+			.bitmask_of_supported_http_methods = BIT(HTTP_GET),
+			.content_encoding = "gzip",
+			.content_type = "text/html",
+		},
+	.static_data = index_html_gz,
+	.static_data_len = sizeof(index_html_gz),
+};
+
+HTTP_RESOURCE_DEFINE(index_html_gz_resource, siot_http_service, "/",
+		     &index_html_gz_resource_detail);
 
 int main(void)
 {
 	LOG_INF("SIOT Zephyr Application! %s", CONFIG_BOARD_TARGET);
-	// http_server_start();
-
-	// const struct device *dac_dev = DEVICE_DT_GET(DAC_DEVICE_NODE);
-
-	// if (!device_is_ready(dac_dev)) {
-	// 	LOG_WRN("DAC device not ready\n");
-	// }
-
-	// int ret = dac_write_value(dac_dev, DAC_CHANNEL, 256);
-	// LOG_DBG("dac write returned: %i", ret);
+	http_server_start();
 
 	return 0;
 }
