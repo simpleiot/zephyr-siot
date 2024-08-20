3390009ca32dc5ef3e59add9129c8c4e91851ab4
Cliff Brake <cbrake@bec-systems.com>
2024-08-20 17:01:19 -0400

    initialize NVS memory and store boot count

--- apps/siot-net/src/main.c
+++ apps/siot-net/src/main.c
@@ -2,8 +2,64 @@
 #include <zephyr/net/http/server.h>
 #include <zephyr/net/http/service.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/flash.h>
+#include <zephyr/storage/flash_map.h>
+#include <zephyr/fs/nvs.h>
 
 LOG_MODULE_REGISTER(siot, LOG_LEVEL_DBG);
+
+// NVS Keys
+#define NVS_KEY_BOOT_CNT 1
+
+static struct nvs_fs fs;
+
+#define NVS_PARTITION        storage_partition
+#define NVS_PARTITION_DEVICE FIXED_PARTITION_DEVICE(NVS_PARTITION)
+#define NVS_PARTITION_OFFSET FIXED_PARTITION_OFFSET(NVS_PARTITION)
+
+int nvs_init()
+{
+	struct flash_pages_info info;
+	int rc = 0;
+	uint32_t reboot_counter = 0U;
+
+	/* define the nvs file system by settings with:
+	 *	sector_size equal to the pagesize,
+	 *	3 sectors
+	 *	starting at NVS_PARTITION_OFFSET
+	 */
+	fs.flash_device = NVS_PARTITION_DEVICE;
+	if (!device_is_ready(fs.flash_device)) {
+		LOG_ERR("Flash device %s is not ready\n", fs.flash_device->name);
+		return -1;
+	}
+	fs.offset = NVS_PARTITION_OFFSET;
+	rc = flash_get_page_info_by_offs(fs.flash_device, fs.offset, &info);
+	if (rc) {
+		LOG_ERR("Unable to get page info\n");
+		return -1;
+	}
+	fs.sector_size = info.size;
+	fs.sector_count = 3U;
+
+	rc = nvs_mount(&fs);
+	if (rc) {
+		LOG_ERR("Flash Init failed\n");
+		return -1;
+	}
+
+	rc = nvs_read(&fs, NVS_KEY_BOOT_CNT, &reboot_counter, sizeof(reboot_counter));
+	if (rc > 0) { /* item was found, show it */
+		LOG_INF("Id: %d, boot_counter: %d\n", NVS_KEY_BOOT_CNT, reboot_counter);
+		reboot_counter++;
+		(void)nvs_write(&fs, NVS_KEY_BOOT_CNT, &reboot_counter, sizeof(reboot_counter));
+	} else { /* item was not found, add it */
+		LOG_INF("No boot counter found, adding it at id %d\n", NVS_KEY_BOOT_CNT);
+		(void)nvs_write(&fs, NVS_KEY_BOOT_CNT, &reboot_counter, sizeof(reboot_counter));
+	}
+
+	return 0;
+}
 
 static uint8_t index_html_gz[] = {
 #include "index.html.gz.inc"
@@ -29,7 +85,11 @@ HTTP_RESOURCE_DEFINE(index_html_gz_resource, siot_http_service, "/",
 
 int main(void)
 {
+	int rc = 0;
+
 	LOG_INF("SIOT Zephyr Application! %s", CONFIG_BOARD_TARGET);
+	nvs_init();
+
 	http_server_start();
 
 	return 0;
