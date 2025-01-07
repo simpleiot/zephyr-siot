f4142b975f98d7ebbf325f6da4f95df9c9421fc4
Cliff Brake <cbrake@bec-systems.com>
2025-01-07 18:08:00 -0500

    fix compile

--- tests/test.c
+++ tests/test.c
@@ -1,8 +1,8 @@
 #include <zephyr/ztest.h>
 
-ZTEST(test_test, test_functionality)
+ZTEST_SUITE(test_tests, NULL, NULL, NULL, NULL, NULL);
+
+ZTEST(test_tests, simple_test)
 {
 	zassert_equal(9, 10, "Expected result is 10");
 }
-
-ZTEST_SUIT(test_test_suit, test_test, NULL, NULL, NULL, NULL);
