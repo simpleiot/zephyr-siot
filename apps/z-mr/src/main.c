2c48f8d793081474ed5c023f8928f32852a2b3cf
Cliff Brake <cbrake@bec-systems.com>
2025-01-17 18:01:58 -0500

    add settings table

--- apps/z-mr/src/main.c
+++ apps/z-mr/src/main.c
@@ -1,12 +1,21 @@
-#include "zephyr/kernel.h"
-#include <stdint.h>
+#include <nvs.h>
+#include <point.h>
 
+#include <stdint.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/zbus/zbus.h>
 #include <app_version.h>
 
 LOG_MODULE_REGISTER(z_mr, LOG_LEVEL_DBG);
+
+// The following points will get persisted in NVS when the show up on
+// zbus point_chan.
+static const struct nvs_point nvs_pts[] = {
+	{1, &point_def_boot_count, "0"}, {2, &point_def_description, "0"},
+	{3, &point_def_staticip, "0"},   {4, &point_def_address, "0"},
+	{5, &point_def_gateway, "0"},    {6, &point_def_netmask, "0"},
+};
 
 // ==================================================
 // Network manager
@@ -24,6 +33,8 @@ static void net_event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_
 int main(void)
 {
 	LOG_INF("Zonit M+R: %s %s", CONFIG_BOARD_TARGET, APP_VERSION_EXTENDED_STRING);
+
+	nvs_init(nvs_pts, ARRAY_SIZE(nvs_pts));
 
 	// In your initialization code:
 	net_mgmt_init_event_callback(&mgmt_cb, net_event_handler, NET_EVENT_L4_CONNECTED);
