23453d4e511483f6c70d32bf84d0f91106a7dcb1
jackn <jackn@zonit.com>
2025-02-24 12:24:35 -0700

    Fixing decoder error

--- apps/z-mr/frontend/src/Shared.elm
+++ apps/z-mr/frontend/src/Shared.elm
@@ -2,11 +2,13 @@ module Shared exposing
     ( Flags
     , Model
     , Msg(..)
+    , decoder
     , init
     , subscriptions
     , update
     )
 
+import Json.Decode as Decode exposing (Decoder)
 import Ports
 import Shared.Model
 
@@ -15,6 +17,13 @@ type alias Flags =
     { width : Int
     , height : Int
     }
+
+
+decoder : Decoder Flags
+decoder =
+    Decode.map2 Flags
+        (Decode.field "width" Decode.int)
+        (Decode.field "height" Decode.int)
 
 
 type alias Model =
